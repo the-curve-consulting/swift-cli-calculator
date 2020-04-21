d393f98e1f6bd8e1c2ebe089713e783ebd192668
Paul Ridgway <paul@ridgway.io>
2020-04-21 11:33:15 +0100

    Initial Commit

---
+++ CalculatorApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalculatorApp
+//
+//  Created by Paul Ridgway on 21/04/2020.
+//  Copyright © 2020 5052. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
